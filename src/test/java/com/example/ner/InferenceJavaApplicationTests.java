ef63d835865aeb007006fba20a715d8fdbb7d855
barisgevher <nostradamiz5@gmail.com>
2025-12-17 20:29:46 +0300

    initial commit: created some of the folders and packages for projects

---
+++ src/test/java/com/example/ner/InferenceJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InferenceJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
